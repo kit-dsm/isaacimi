ec1fe5f053e0ab771af01a991f1fbc92808e93a2
David <fl2644@partner.kit.edu>
2025-06-25 17:59:07 +0200

    wip, enable scene configuration with a yaml file

--- scripts/scene_runner.py
+++ scripts/scene_runner.py
@@ -1,76 +1,41 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--config',
+    type=str,
+    required=True,
+    help='Path to the scene configuration file' # for now, path is relative to where the script is called
+)
+args = parser.parse_args()
+
+import yaml
+with open(args.config, 'r') as scene_config_file:
+    scene_config = yaml.safe_load(scene_config_file)
+
 from isaacsim import SimulationApp
+app_config = {
+    "headless": scene_config["app"]["headless"],
+    "renderer": scene_config["app"]["renderer"],
+}
+simulation_app = SimulationApp(app_config)
 
-simulation_app = SimulationApp({"headless": False})
-
-import sys
+from isaacsim.core.api import World
+world = World(scene_config["world"]["stage_units_in_meters"])
+world.set_simulation_dt(physics_dt=scene_config["world"]["physics_dt"], rendering_dt=scene_config["world"]["rendering_dt"])
 
 import carb
-import numpy as np
-from isaacsim.core.api import World
-from isaacsim.core.prims import Articulation
-from isaacsim.core.utils.stage import add_reference_to_stage, get_stage_units
-from isaacsim.core.utils.types import ArticulationAction
-from isaacsim.core.utils.viewports import set_camera_view
+import sys
 from isaacsim.storage.native import get_assets_root_path
-
-# preparing the scene
 assets_root_path = get_assets_root_path()
 if assets_root_path is None:
     carb.log_error("Could not find Isaac Sim assets folder")
     simulation_app.close()
     sys.exit()
 
-my_world = World(stage_units_in_meters=1.0)
-my_world.scene.add_default_ground_plane()  # add ground plane
-set_camera_view(
-    eye=[5.0, 0.0, 1.5], target=[0.00, 0.00, 1.00], camera_prim_path="/OmniverseKit_Persp"
-)  # set camera view
+from isaacsim.core.utils.stage import add_reference_to_stage
+add_reference_to_stage(usd_path=assets_root_path+scene_config["scene"]["environment_usd_path"], prim_path="/World/Environment")
 
-# Add Franka
-asset_path = assets_root_path + "/Isaac/Robots/Franka/franka.usd"
-add_reference_to_stage(usd_path=asset_path, prim_path="/World/Arm")  # add robot to stage
-arm = Articulation(prim_paths_expr="/World/Arm", name="my_arm")  # create an articulation object
-
-# Add Carter
-asset_path = assets_root_path + "/Isaac/Robots/NVIDIA/Carter/nova_carter/nova_carter.usd"
-add_reference_to_stage(usd_path=asset_path, prim_path="/World/Car")
-car = Articulation(prim_paths_expr="/World/Car", name="my_car")
-
-# Add dummy robot
-asset_path = "/home/fl2644/KIT/evorobot-isaacsim/dummy.usd" # hardcode the path here for now
-add_reference_to_stage(usd_path=asset_path, prim_path="/World/Dummy")
-dummy = Articulation(prim_paths_expr="/World/Dummy", name="my_dummy_robot")
-
-# set the initial poses of the arm and the car so they don't collide BEFORE the simulation starts
-arm.set_world_poses(positions=np.array([[0.0, 1.0, 0.0]]) / get_stage_units())
-car.set_world_poses(positions=np.array([[0.0, -1.0, 0.0]]) / get_stage_units())
-dummy.set_world_poses(positions=np.array([[3.0, 3.0, 0.0]]) / get_stage_units())
-
-
-# initialize the world
-my_world.reset()
-
-for i in range(4):
-    print("running cycle: ", i)
-    if i == 1 or i == 3:
-        print("moving")
-        # move the arm
-        arm.set_joint_positions([[-1.5, 0.0, 0.0, -1.5, 0.0, 1.5, 0.5, 0.04, 0.04]])
-        # move the car
-        car.set_joint_velocities([[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]])
-    if i == 2:
-        print("stopping")
-        # reset the arm
-        arm.set_joint_positions([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
-        # stop the car
-        car.set_joint_velocities([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
-
-    for j in range(100):
-        # step the simulation, both rendering and physics
-        my_world.step(render=True)
-        # print the joint positions of the car at every physics step
-        if i == 3:
-            car_joint_positions = car.get_joint_positions()
-            print("car joint positions:", car_joint_positions)
+for robot in scene_config["scene"]["robots"]:
+    print("spawning robot")
 
 simulation_app.close()
