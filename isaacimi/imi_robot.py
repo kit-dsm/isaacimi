488d222903955505cbd1b36995864cba94e4bff3
David <fl2644@partner.kit.edu>
2025-07-01 12:17:01 +0200

    Add method definitions for creating ros publishers and subscribers

--- isaacimi/imi_robot.py
+++ isaacimi/imi_robot.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Callable, Union, TypeVar, Any, TYPE_CHECKING
 import numpy as np
 from abc import ABC, abstractmethod
 
@@ -6,6 +6,15 @@ from isaacsim.core.api.tasks import BaseTask
 from isaacsim.core.api.scenes.scene import Scene
 from isaacsim.core.api.robots import Robot
 from isaacsim.core.utils.stage import add_reference_to_stage
+
+import rclpy
+from rclpy.node import Node
+
+if TYPE_CHECKING:
+    from rclpy.callback_groups import CallbackGroup
+    from rclpy.qos import QoSProfile
+    from rclpy.subscription import Subscription
+    from rclpy.publisher import Publisher
 
 class ImiRobot(BaseTask):
     def __init__(
@@ -23,6 +32,7 @@ class ImiRobot(BaseTask):
         self._robot_usd_path = usd_path
         self._robot_initial_position = position
         self._robot_initial_orientation = orientation
+        self._ros_node = None
         return
     
     def set_up_scene(self, scene: Scene) -> None:
@@ -43,3 +53,31 @@ class ImiRobot(BaseTask):
         self._robot.set_linear_velocity(np.array([0, 0, 0]))
         self._robot.set_local_pose(self._robot_initial_position, self._robot_initial_orientation)
         return
+    
+    def pre_step(self, time_step_index: int, simulation_time: float) -> None:
+        # todo
+        return
+    
+    def _get_ros_node(self) -> Node:
+        if self._ros_node is None:
+            self.ros_node = rclpy.create_node(self._robot_name + "_node")
+        return self._ros_node
+            
+    def add_ros_subscriber(
+        self,
+        msg_type,
+        topic: str,
+        callback: Callable[[Any], None],
+        qos_profile: Union['QoSProfile', int],
+        *,
+        callback_group: Optional['CallbackGroup'] = None,
+    ) -> 'Subscription':
+        return self._get_ros_node().create_subscription(msg_type, topic, callback, qos_profile)
+
+    def add_ros_publisher(
+        self,
+        msg_type,
+        topic: str,
+        qos_profile: Union['QoSProfile', int],
+    ) -> 'Publisher':
+        return self._get_ros_node().create_publisher(msg_type, topic, qos_profile)
