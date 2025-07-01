cc8077997bf89feac36acec2d0d7efa74f3d8e01
David <fl2644@partner.kit.edu>
2025-07-01 10:18:22 +0200

    Small fix: Correct spelling to set_up_scene, add reference to stage before setting robot

--- isaacimi/imi_robot.py
+++ isaacimi/imi_robot.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 from isaacsim.core.api.tasks import BaseTask
 from isaacsim.core.api.scenes.scene import Scene
 from isaacsim.core.api.robots import Robot
+from isaacsim.core.utils.stage import add_reference_to_stage
 
 class ImiRobot(BaseTask):
     def __init__(
@@ -24,12 +25,13 @@ class ImiRobot(BaseTask):
         self._robot_initial_orientation = orientation
         return
     
-    def setup_scene(self, scene: Scene):
+    def set_up_scene(self, scene: Scene):
         super().set_up_scene(scene)
+        add_reference_to_stage(usd_path=self._robot_usd_path, prim_path=self._robot_prim_path)
         self.set_robot()
-        scene.add(self._robot)
         return
     
     def set_robot(self) -> Robot:
-        self._robot = Robot(prim_path=self._robot_prim_path, name=self._robot_name, position=self._robot_initial_position, orientation=self._robot_initial_orientation, articulation_controller=None)
+        if self._robot is None:
+            self._robot = Robot(prim_path=self._robot_prim_path, name=self._robot_name, position=self._robot_initial_position, orientation=self._robot_initial_orientation, articulation_controller=None)
         return self._robot
