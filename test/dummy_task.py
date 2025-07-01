9457b641d5c091c25c13334b0ad311e154a5a16b
David <fl2644@partner.kit.edu>
2025-07-01 15:50:58 +0200

    Add velocity_callback and custom_pre_step to custom task

--- test/dummy_task.py
+++ test/dummy_task.py
@@ -2,6 +2,8 @@ from isaacimi import ImiRobot
 
 from geometry_msgs.msg import Twist
 
+import numpy as np
+
 class DummyTask(ImiRobot):
     def __init__(
         self,
@@ -13,8 +15,20 @@ class DummyTask(ImiRobot):
     ):
         super().__init__(prim_path, name, usd_path, position, orientation)
         self.add_ros_subscriber(Twist, "cmd_vel", self.velocity_callback, 1)
+        self.lin_vel = np.zeros(3)
+        self.ang_vel = np.zeros(3)
         return
     
     def velocity_callback(self, data: Twist) -> None:
-        print("Velocity callback!")
+        self.lin_vel[0] = data.linear.x
+        self.lin_vel[1] = data.linear.y
+        self.lin_vel[2] = data.linear.z
+        self.ang_vel[0] = data.angular.x
+        self.ang_vel[1] = data.angular.y
+        self.ang_vel[2] = data.angular.z
+        return
+    
+    def custom_pre_step(self) -> None:
+        self._robot.set_linear_velocity(self.lin_vel)
+        self._robot.set_angular_velocity(self.ang_vel)
         return
