fb0635194a17fa233564c2fe088caf4cfb309ffd
David <fl2644@partner.kit.edu>
2025-07-06 15:52:28 +0200

    Implement dummy robot plugin with new plugin interface

--- examples/robots/dummy_robot/dummy_robot_plugin.py
+++ examples/robots/dummy_robot/dummy_robot_plugin.py
@@ -5,43 +5,42 @@ from std_msgs.msg import Int8
 
 import numpy as np
 
-class DummyRobotPlugin(ImiRobotPlugin):
-    def __init__(self):
-        # self.add_ros_subscriber(Twist, "cmd_vel", self.velocity_callback, 1)
-        # self.lin_vel = np.zeros(3)
-        # self.ang_vel = np.zeros(3)
+class DummyRobotPlugin(ImiRobotPlugin):   
+    def on_plugin_load(self):
+        print("Dummy plugin loaded onto robot!")
+        self.ros_node.create_subscription(Twist, "cmd_vel", self.velocity_callback, 1)
+        self.lin_vel = np.zeros(3)
+        self.ang_vel = np.zeros(3)
 
-        # self.add_ros_subscriber(Int8, "cmd_lift", self.lift_callback, 1)
-        # self.cmd_lift = 0 # either -1, 0, or +1
+        self.ros_node.create_subscription(Int8, "cmd_lift", self.lift_callback, 1)
+        self.cmd_lift = 0 # either -1, 0, or +1
 
-        # self.max_lift_moving_speed = 0.25
-        # self.lift_active = False
-        print("DUMMY PLUGIN CONSTRUCTOR")
-        
+        self.max_lift_moving_speed = 0.25
+        self.lift_active = False    
         return
-    
-    # def velocity_callback(self, data: Twist) -> None:
-    #     self.lin_vel[0] = data.linear.x
-    #     self.lin_vel[1] = data.linear.y
-    #     self.lin_vel[2] = data.linear.z
-    #     self.ang_vel[0] = data.angular.x
-    #     self.ang_vel[1] = data.angular.y
-    #     self.ang_vel[2] = data.angular.z
-    #     return
 
-    # def lift_callback(self, data: Int8) -> None:
-    #     self.cmd_lift = data.data
+    def velocity_callback(self, data: Twist) -> None:
+        self.lin_vel[0] = data.linear.x
+        self.lin_vel[1] = data.linear.y
+        self.lin_vel[2] = data.linear.z
+        self.ang_vel[0] = data.angular.x
+        self.ang_vel[1] = data.angular.y
+        self.ang_vel[2] = data.angular.z
+        return
+
+    def lift_callback(self, data: Int8) -> None:
+        self.cmd_lift = data.data
     
-    # def custom_pre_step(self) -> None:
-    #     if self.cmd_lift == 0:
-    #         self.lift_active = False
-    #         self._robot.set_linear_velocity(self.lin_vel)
-    #         self._robot.set_angular_velocity(self.ang_vel)
-    #     else:
-    #         self.lift_active = True
-    #         if self.cmd_lift > 0:
-    #             self._robot.set_joint_velocities(np.array([+self.max_lift_moving_speed]))
-    #         else:
-    #             self._robot.set_joint_velocities(np.array([-self.max_lift_moving_speed]))
+    def on_physics_step(self, robot, step_size) -> None:
+        if self.cmd_lift == 0:
+            self.lift_active = False
+            robot.set_linear_velocity(self.lin_vel)
+            robot.set_angular_velocity(self.ang_vel)
+        else:
+            self.lift_active = True
+            if self.cmd_lift > 0:
+                robot.set_joint_velocities(np.array([+self.max_lift_moving_speed]))
+            else:
+                robot.set_joint_velocities(np.array([-self.max_lift_moving_speed]))
             
-    #     return
+        return
