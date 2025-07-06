8f6f18e04de350714109b7c7f2d5a7df0f3692e5
David <fl2644@partner.kit.edu>
2025-07-06 15:18:58 +0200

    Make small changes to variable names in ImiRobotTask for clarity, call on_plugin_load when a plugin is added

--- src/isaacimi/robot_task.py
+++ src/isaacimi/robot_task.py
@@ -10,41 +10,44 @@ from .imi_robot import ImiRobot
 
 class ImiRobotTask(BaseTask):
     def __init__(self, robot: ImiRobot, robot_plugins: List[ImiRobotPlugin] = []) -> None:
-        super().__init__(name=robot.get_name()+"_task", offset=None)
+        super().__init__(name=f"{robot.name}_task", offset=None)
         self._robot = robot
-        self.plugins = robot_plugins
+        self._plugins = robot_plugins
+        for plugin in self._plugins:
+            plugin.on_plugin_load()
         return
     
     def add_plugin(self, plugin: ImiRobotPlugin) -> None:
-        self.plugins.append(plugin)
+        self._plugins.append(plugin)
+        plugin.on_plugin_load()
         return
 
     def set_up_scene(self, scene: Scene):
         super().set_up_scene(scene)
         scene.add(self._robot)
-        for plugin in self.plugins:
+        for plugin in self._plugins:
             plugin.set_up_scene(scene)
         return
     
     def post_reset(self) -> None:
-        for plugin in self.plugins:
+        for plugin in self._plugins:
             plugin.post_reset()
         return
 
     def cleanup(self) -> None:
-        for plugin in self.plugins:
+        for plugin in self._plugins:
             plugin.cleanup()
         return
     
-    def pre_step(self) -> None:
+    def pre_step(self, time_step_index: int, simulation_time: float) -> None:
         # todo: spin ros_nodes
-        for plugin in self.plugins:
-            plugin.pre_physics_step(self._robot)
+        for plugin in self._plugins:
+            plugin.pre_physics_step(self._robot, time_step_index, simulation_time)
         return
 
     def on_physics_step(self, step_size: float) -> None:
-        for plugin in self.plugins:
-            plugin.post_physics_step(self._robot, step_size)
+        for plugin in self._plugins:
+            plugin.on_physics_step(self._robot, step_size)
         return
 
     
