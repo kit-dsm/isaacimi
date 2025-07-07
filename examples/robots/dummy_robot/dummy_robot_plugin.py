dddf5a0ee92ee097a854debd1d9d21bbb636c689
David <fl2644@partner.kit.edu>
2025-07-07 11:59:15 +0200

    Add an example lidar plugin

--- examples/robots/dummy_robot/dummy_robot_plugin.py
+++ examples/robots/dummy_robot/dummy_robot_plugin.py
@@ -5,18 +5,17 @@ from std_msgs.msg import Int8
 
 import numpy as np
 
-class DummyRobotPlugin(ImiRobotPlugin):   
+class DummyRobotControllerPlugin(ImiRobotPlugin):   
     def on_plugin_load(self):
-        print("Dummy plugin loaded onto robot!")
         self.ros_node.create_subscription(Twist, "cmd_vel", self.velocity_callback, 1)
         self.lin_vel = np.zeros(3)
         self.ang_vel = np.zeros(3)
 
         self.ros_node.create_subscription(Int8, "cmd_lift", self.lift_callback, 1)
-        self.cmd_lift = 0 # either -1, 0, or +1
+        self.lift_direction = 0 # either -1, 0, or +1
 
         self.max_lift_moving_speed = 0.25
-        self.lift_active = False    
+        self.lift_active = False
         return
 
     def velocity_callback(self, data: Twist) -> None:
@@ -29,18 +28,58 @@ class DummyRobotPlugin(ImiRobotPlugin):
         return
 
     def lift_callback(self, data: Int8) -> None:
-        self.cmd_lift = data.data
+        self.lift_direction = data.data
     
     def on_physics_step(self, robot, step_size) -> None:
-        if self.cmd_lift == 0:
+        if self.lift_direction == 0:
             self.lift_active = False
             robot.set_linear_velocity(self.lin_vel)
             robot.set_angular_velocity(self.ang_vel)
         else:
             self.lift_active = True
-            if self.cmd_lift > 0:
+            if self.lift_direction > 0:
                 robot.set_joint_velocities(np.array([+self.max_lift_moving_speed]))
             else:
                 robot.set_joint_velocities(np.array([-self.max_lift_moving_speed]))
-            
+        return
+    
+
+import omni
+from pxr import Gf
+import omni.replicator.core as rep
+class DummyRobotLidarPlugin(ImiRobotPlugin):
+    def on_plugin_load(self):
+        # Create the lidar sensor that generates data into "RtxSensorCpu"
+        # Sensor needs to be rotated 90 degrees about X so that its Z up
+
+        # Possible options are Example_Rotary and Example_Solid_State
+        # drive sim applies 0.5,-0.5,-0.5,w(-0.5), we have to apply the reverse
+        _, sensor = omni.kit.commands.execute(
+            "IsaacSensorCreateRtxLidar",
+            path="/microscan",
+            parent="/World/dummy1/base_footprint/base_link/fr_lidar",
+            config="SICK_microscan3_ABAZ90ZA1P01", # you need to modify the app.sensors.nv.lidar.profileBaseFolder setting
+                                                   # in the isaacsim/exts/isaacsim.sensors.rtx/config/extension.toml file to 
+                                                   # add your own lidar config, as explained here: https://forums.developer.nvidia.com/t/add-search-path-for-lidar-configs/255352/2
+            translation=(0.05, 0, 0),
+            orientation=Gf.Quatd(1, 0, 0, 0),  # Gf.Quatd is w,i,j,k
+        )
+
+        # RTX sensors are cameras and must be assigned to their own render product
+        hydra_texture = rep.create.render_product(sensor.GetPath(), [1, 1], name="Isaac")
+
+        # # Create Point cloud publisher pipeline in the post process graph
+        writer = rep.writers.get("RtxLidar" + "ROS2PublishPointCloud")
+        writer.initialize(topicName="point_cloud", frameId="base_scan")
+        writer.attach([hydra_texture])
+
+        # Create the debug draw pipeline in the post process graph
+        writer = rep.writers.get("RtxLidar" + "DebugDrawPointCloud")
+        writer.attach([hydra_texture])
+
+        # Create LaserScan publisher pipeline in the post process graph
+        writer = rep.writers.get("RtxLidar" + "ROS2PublishLaserScan")
+        writer.initialize(topicName="scan", frameId="base_scan")
+        writer.attach([hydra_texture])
+
         return
