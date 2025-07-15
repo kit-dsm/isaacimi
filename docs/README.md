e0b581d4d8215665ca1a7db2147bb587406d4136
David <fl2644@partner.kit.edu>
2025-07-15 14:25:01 +0200

    Update README.md and update serve.sh in docs/

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# Building the documentation
+
+We use [mkdocs](https://www.mkdocs.org/) with the [mkdocs-material](https://squidfunk.github.io/mkdocs-material/) theme, along with various plugins to create our documentation.
+
+To view the docs locally, run the `serve.sh` script to install the pip packages and start the built-in dev server. We recommend creating a Pyhton virtual environment before running the script.
--- docs/serve.sh
+++ docs/serve.sh
@@ -3,10 +3,22 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${SCRIPT_DIR}
 
+check_pip() {
+    for PACKAGE_NAME in "$@"; do
+        if ! python3 -m pip show $PACKAGE_NAME &> /dev/null; then
+            if ! python3 -m pip install $PACKAGE_NAME; then
+                echo "Failed to install $PACKAGE_NAME via pip"
+                exit 1
+            fi
+        fi
+    done
+}
 
-if ! python3 -m pip show mkdocs-material &> /dev/null; then
-    if ! python3 -m pip install mkdocs-material; then
-        echo "Failed to install mkdocs-material via pip"
+check_pip mkdocs-material mkdocstrings-python mkdocs-gen-files mkdocs-literate-nav mkdocs-section-index
+
+if ! python3 -m pip show isaacimi &> /dev/null; then
+    if ! python3 -m pip install -e ..; then
+        echo "Failed to install isaacimi via pip"
         exit 1
     fi
 fi
@@ -17,6 +29,3 @@ fi
 
 mkdocs serve
 exit $?
-
-
-
