84ce45fea48976afac14577e6b97f013e9875518
David <fl2644@partner.kit.edu>
2025-07-23 16:18:11 +0200

    Rename api reference generation script to be more specific

--- docs/scripts/gen_api_ref_pages.py
+++ docs/scripts/gen_api_ref_pages.py
@@ -12,7 +12,7 @@ src = root / "src"
 for path in sorted(src.rglob("*.py")):  
     module_path = path.relative_to(src).with_suffix("")  
     doc_path = path.relative_to(src).with_suffix(".md")  
-    full_doc_path = Path("reference", doc_path)  
+    full_doc_path = Path("reference", "api", doc_path)
 
     parts = tuple(module_path.parts)
 
@@ -31,5 +31,5 @@ for path in sorted(src.rglob("*.py")):
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:  
+with mkdocs_gen_files.open("reference/api/SUMMARY.md", "w") as nav_file:  
     nav_file.writelines(nav.build_literate_nav()) 
